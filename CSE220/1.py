105a3e03b824b9256e1a5c3c3a609a765a5c0a89
HpnoSphynx <asm.zawadul.karim@g.bracu.ac.bd>
2023-01-28 02:15:32 +0600

    v.1

---
+++ CSE220/1.py
@@ -0,0 +1,10 @@
+def shiftLeft(source,k):
+
+    new_array=[0]*len(source)
+
+    for i in range(0,len(source)-k): 
+        new_array[i]=source[k]
+        k+=1  
+    print(new_array)
+source=[10,20,30,40,50,60]
+shiftLeft(source,3)
