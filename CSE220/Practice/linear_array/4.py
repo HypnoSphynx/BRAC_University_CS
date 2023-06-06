ffe416ce4f52fa2a96be16d1d37b35e54d783dde
HypnoSphynx <123151627+HypnoSphynx@users.noreply.github.com>
2023-06-07 02:48:53 +0600

    week2_summer23

--- CSE220/Practice/linear_array/4.py
+++
@@ -1,8 +0,0 @@
-def freq_check(array,n,x):
-    count=0
-    for i in range(n):
-        if array[i]==x:
-            count+=1
-    
-    return count
-print(freq_check([1,1,1,1,1],5,1))
---
+++ CSE220/key_index_searching/3.py
@@ -0,0 +1,3 @@
+import kk
+
+print(kk.a)
